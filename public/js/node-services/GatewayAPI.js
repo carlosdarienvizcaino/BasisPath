1eadc22e49367221e886baaa9bf0a8c87e029112
Carlos Vizcaino <carlos.vizcaino@ufl.edu>
2016-06-13 22:57:49 -0400

    Removes console.log()

--- public/js/node-services/GatewayAPI.js
+++ public/js/node-services/GatewayAPI.js
@@ -1,11 +1,17 @@
 
 // public/js/node-services/GatewayAPI.js
 
+var uuid = require('uuid');
+
 function handleRequest(req,res){
-    
+   
+    var id = uuid.v1();
+       
     // Check for req body code-type
-    res.status(201).send("Request was received");
+    res.status(201).send(id);
+    console.log("Hello");
 }
+
 module.exports = {
     
    handleRequest : handleRequest
