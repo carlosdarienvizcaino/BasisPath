781efeef968d81a8acf82dfadb89e6ec6dd73a79
Carlos Vizcaino <Carlos_Vizcaino@ultimatesoftware.com>
2016-08-16 18:04:06 -0400

    Vagrant and docker is not working

---
+++ deployment/java-parser-service/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Constants
+const PORT = 3006;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Java Parser\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
